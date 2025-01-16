86d16f4e301a17403a6fbef27c2ca69b581a6236
abram <abrambagus@gmail.com>
2025-01-16 17:32:35 +0700

    add vue value app

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel",
+          phone: "02130130123",
+          email: "manuel@gmal.com",
+        },
+        {
+          id: "julia",
+          name: "Julia",
+          phone: "02130130123",
+          email: "julia@gmal.com",
+        },
+      ],
+    };
+  },
+};
+</script>
